e7cf5e57027e02639fc368e83558b4fbb1fff7b8
Kartavya Raikwar <kartvayaraikwar@gmail.com>
2026-02-10 17:07:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-import streamlit as st
-import pandas as pd
 import os
 import joblib
 
-# -----------------------------
-# Resolve paths safely
-# -----------------------------
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 model_path = os.path.join(BASE_DIR, "model.pkl")
