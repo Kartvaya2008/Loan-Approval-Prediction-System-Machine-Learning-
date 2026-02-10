dfbd7eac1c51190962cf7b5b75ef105aa2bea92a
Kartavya Raikwar <kartvayaraikwar@gmail.com>
2026-02-10 16:52:16 +0530

    Update app.py

--- Loan Approval Prediction/app.py
+++ Loan Approval Prediction/app.py
@@ -1,6 +1,11 @@
-import joblib
+import streamlit as st
+import pandas as pd
 import os
+import joblib
 
+# -----------------------------
+# Resolve paths safely
+# -----------------------------
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 model_path = os.path.join(BASE_DIR, "model.pkl")
